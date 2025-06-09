c83439bab9c2036d64cab9d8d7e18bd32227a19d
SonHoang <120953254+sonhoang002@users.noreply.github.com>
2025-06-09 15:01:31 -0400

    New script, index, and stylesheet

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let humanChoice;
 let compScore = 0;
 let humanScore = 0;
 
+const newContainer = document.createElement('div');
+newContainer.classList.add('newCont');
+
+
+
+// Random choice for computer
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3);
 
@@ -17,19 +23,55 @@ function getComputerChoice() {
     return compChoice;
 }
 
-function getHumanChoice() {
-    let choose = prompt("Rock(1), Paper(2), or Scissor(3) ?");
-    if (parseInt(choose) === 1) {
-        humanChoice = "Rock";
-    } else if (parseInt(choose) === 2) {
-        humanChoice = "Paper";
-    } else {
-        humanChoice = "Scissor";
-    }
+// Create the 3 button (Rock, paper, scissor)
+function createButtons() {
+    const rock1 = document.createElement('button');
+    const paper1 = document.createElement('button');
+    const scissor1 = document.createElement('button');
 
-    return humanChoice;
+    rock1.classList.add('chooseRock');
+    paper1.classList.add('choosePaper');
+    scissor1.classList.add('chooseScissor');
+
+    rock1.textContent = "Rock";
+    paper1.textContent = "Paper";
+    scissor1.textContent = "Scissor";
+
+    let arr = [rock1, paper1, scissor1];
+    return arr;
 }
 
+// Load the button when the DOM loaded
+document.addEventListener('DOMContentLoaded', () => {
+    let arr = createButtons();
+    newContainer.append(arr[0], arr[1], arr[2]);
+    document.body.append(newContainer, outcome, scoreContainer, paraContainer);
+    // document.body.appendChild(outcome);
+    // document.body.appendChild(scoreContainer);
+    // document.body.appendChild(paraContainer);
+
+    newContainer.addEventListener('click', (event) => {
+        let target = event.target;
+        computer = getComputerChoice();
+
+        switch(target.className) {
+            case 'chooseRock':
+                human = "Rock";
+                playRound(computer, human);
+                break;
+            case 'choosePaper':
+                human = "Paper";
+                playRound(computer, human);
+                break;
+            case 'chooseScissor':
+                human = "Scissor";   
+                playRound(computer, human);
+                break;
+        }
+    });
+});
+
+// Logic of the game
 function playRound(computer, human) {
     switch(computer) {
         case "Rock":
@@ -73,29 +115,53 @@ function playRound(computer, human) {
             }
     }
 
-    console.log("Your pick: " + human);
-    console.log("Computer pick: " + computer);
-    console.log("Your score: " + humanScore + " : Computer Score: " + compScore);
+    scores.textContent =  humanScore + " --- " + compScore;
+    yourPara.textContent = "Your choice: " + human;
+    compPara.textContent = "Computer Choice: " + computer;
 
-}
-
-function playGame(num) {
-    let count = 0;
-    while (count < num) {
-        const humanSelection = getHumanChoice();
-        const compSelection = getComputerChoice();
-        playRound(compSelection, humanSelection);
-        count ++;
-    }
-
-    if (humanScore === compScore) {
-        console.log("This match end in a draw!");
-    } else if (humanScore > compScore) {
-        console.log("You win the match!");
-    } else {
-        console.log("You lose the match!");
+    if (humanScore === 5) {
+        outcome.style.height = "200px";
+        outcome.textContent = "YOU WIN!";
+        newContainer.remove();
+        document.body.appendChild(resetCont);
+    } else if (compScore === 5) {
+        outcome.style.height = "200px";
+        outcome.textContent = "YOU LOSE!";
+        newContainer.remove();
+        document.body.appendChild(resetCont);
     }
 }
+// Show Winner
+const outcome = document.createElement('div');
+outcome.classList.add('outcome');
 
-let time = prompt("How many times?");
-playGame(parseInt(time));
+// Show current score
+const scoreContainer = document.createElement('div');
+scoreContainer.classList.add('scoreCont');
+
+const scores = document.createElement('div');
+
+scoreContainer.append(scores);
+
+
+// Show Decision
+const paraContainer = document.createElement('div');
+paraContainer.classList.add('paraCont');
+
+const yourPara = document.createElement('div');
+const compPara = document.createElement('div');
+
+paraContainer.append(yourPara, compPara);
+
+// Reset button with the container containing it
+const resetBtn = document.createElement("button");
+resetBtn.classList.add("resetBtn");
+resetBtn.textContent = "Reset!";
+
+resetBtn.addEventListener('click', () => {
+    location.reload();
+});
+
+const resetCont = document.createElement('div');
+resetCont.classList.add('resetCont');
+resetCont.appendChild(resetBtn)
